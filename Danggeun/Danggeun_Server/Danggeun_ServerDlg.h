ce7804d7878384d8e5cf21afeccf5c3730f70f63
cmoweg <cmoweg@gmail.com>
2021-01-09 12:42:09 +0900

    add Server

---
+++ Danggeun/Danggeun_Server/Danggeun_ServerDlg.h
@@ -0,0 +1,34 @@
+﻿
+// Danggeun_ServerDlg.h : header file
+//
+
+#pragma once
+
+
+// CDanggeunServerDlg dialog
+class CDanggeunServerDlg : public CDialogEx
+{
+// Construction
+public:
+	CDanggeunServerDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DANGGEUN_SERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
